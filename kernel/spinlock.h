45b77fc38748df417200502f7f00053a00901502
yahirduran <144289064+yahirduran@users.noreply.github.com>
2025-12-07 17:08:35 -0700

    Update spinlock.h

--- kernel/spinlock.h
+++ kernel/spinlock.h
@@ -6,4 +6,17 @@ struct spinlock {
   char *name;        // Name of lock.
   struct cpu *cpu;   // The cpu holding the lock.
 };
+// Counting semaphore
+struct semaphore {
+  struct spinlock lock;  // semaphore lock
+  int count;             // semaphore value
+  int valid;             // 1 if this entry is in use
+};
 
+// OS semaphore table type
+struct semtab {
+  struct spinlock lock;        // lock protecting the table
+  struct semaphore sem[NSEM];  // fixed-size array of semaphores
+};
+
+extern struct semtab semtable;
